f8bf2c9496ee1f283409dd55502ebdf1007ea860
Jason Li <jiasen.cs08g@nctu.edu.tw>
2021-01-05 15:25:41 +0800

    update README

--- src/train.sh
+++ src/train.sh
@@ -19,13 +19,24 @@
 #                 --save "VRDLx3" \
 #                 --pre_train "../experiment/VRDLx2/model/model_best.pt"
 
-#VRDLx3 + dilated convolurion
-python3 main.py --epochs 300 \
-                --scale 3 \
-                --patch_size 15 \
-                --data_range '1-285/286-291' \
-                --decay "10-20-30-40-100-200" \
-                --save "VRDLx3_Dila" \
-                --dilation
+#VRDLx3_MSE
+# python3 main.py --epochs 100 \
+#                 --scale 3 \
+#                 --patch_size 15 \
+#                 --data_range '1-285/286-291' \
+#                 --decay "10-20-30-40-100-200" \
+#                 --save "VRDLx3_MSE" \
+#                 --pre_train "../experiment/VRDLx3/model/model_best.pt" \
+#                 --loss '1*MSE' \
+#                 --reset
+
+#VRDLx3 + dilated convolution
+# python3 main.py --epochs 300 \
+#                 --scale 3 \
+#                 --patch_size 15 \
+#                 --data_range '1-285/286-291' \
+#                 --decay "10-20-30-40-100-200" \
+#                 --save "VRDLx3_Dila" \
+#                 --dilation
 
 # --self_ensemble
